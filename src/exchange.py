75b03b60413566e363576c46c3f3065ad9018ff2
XnLogicaL <140826629+XnLogicaL@users.noreply.github.com>
2023-12-13 12:00:07 +0300

    Update exchange.py

--- src/exchange.py
+++ src/exchange.py
@@ -1,25 +1,28 @@
-# Author: XnLogicaL
-# Licensed under the MIT License
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class api:
     def get_url(base, to):
-        return "https://www.google.com/finance/quote/%s-%s" % (base, to) # Format url with the provided base and to
+        return "https://www.google.com/finance/quote/%s-%s" % (base, to)
 
-    def get_data(url: str):
-        print("getting latest exchange rates")
+    def get_data(base, to):
+        print(f"getting latest exchange rates")
         
         options = webdriver.FirefoxOptions()
-        options.add_argument("--headless") # Makes the browser invisible.
-        browser = webdriver.Firefox(options=options) # Using chrome might be faster, don't forget to change browser options.
-        browser.get(url=url)
+        options.add_argument("--headless")
+        browser = webdriver.Firefox(options=options)
+        browser.get(url=api.get_data(base, to))
         
         try:
-            return float(browser.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[4]/div/main/div[2]/div[1]/div[1]/c-wiz/div/div[1]/div/div[1]/div/div[1]/div/span/div/div").text) # While XPATH is more reliable, I believe it's a lot slower than class or id.
+            return float(browser.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[4]/div/main/div[2]/div[1]/div[1]/c-wiz/div/div[1]/div/div[1]/div/div[1]/div/span/div/div").text)
         finally:
             browser.quit()
+            
+    def convert(amount, base, to):
+        return api.get_data(base, to) * amount
 
 def get_latest(base: str, to: str):
-    return api.get_data(api.get_url(base, to))
+    return api.get_data(base, to)
+
+def convert(amount: int, base: str, to):
+    return api.convert(amount, base, to)
